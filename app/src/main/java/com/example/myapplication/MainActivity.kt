278f97dd6da036860d4b7223354333e9db728f5d
shoaib <mohshoaibkhan0@gmail.com>
2021-02-26 16:43:55 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        submit.setOnClickListener {
+           
+            if (TextUtils.isEmpty(email.getText())) {
+                password.setError("password is requires!")
+                email.setError("email is required!")
+            } else {
+                val intent = Intent(this,SubmitPage::class.java)
+                startActivity(intent)
+
+        }
+
+    }
+}}
